9d2cc023fe3d76fe69c87d0e91c59d79b79c7f76
Prototype206 <ulyanow.mischa2017@yandex.ru>
2026-06-27 14:07:45 +0400

    Setting up a project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Prototype206/java-project-78/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Prototype206/java-project-78/actions)
+
+### Java CI with Gradle:
+[![Java CI](https://github.com/Prototype206/java-project-78/actions/workflows/ci.yml/badge.svg)](https://github.com/Prototype206/java-project-78/actions/workflows/ci.yml)
+
+### SonarQube:
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=Prototype206_java-project-78&metric=alert_status)](https://sonarcloud.io/dashboard?id=Prototype206_java-project-78)
+[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=Prototype206_java-project-78&metric=coverage)](https://sonarcloud.io/dashboard?id=Prototype206_java-project-78)
---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("java")
+    id("checkstyle")
+    id("jacoco")
+    id("org.sonarqube") version "7.3.1.8318"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.25.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
+    }
+}
+
+checkstyle {
+    toolVersion = "10.13.0"
+    configFile = file("config/checkstyle/checkstyle.xml")
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Prototype206_java-project-78")
+        property("sonar.organization", "prototypes-organization")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.coverage.jacoco.xmlReportPaths", "${layout.buildDirectory.get()}/reports/jacoco/test/xml")
+    }
+}
